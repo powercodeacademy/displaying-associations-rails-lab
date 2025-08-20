d8622137a9627021e3250ee295ca676dbdda5b66
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-20 14:03:18 -0400

    Show and index actions

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,9 @@
 class Artist < ApplicationRecord
+
+   def show 
+  end
+
+  def index 
+  end
+  
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,10 @@
 class Song < ApplicationRecord
   belongs_to :artist
+
+  def show 
+  end
+
+  def index 
+  end
+  
 end
