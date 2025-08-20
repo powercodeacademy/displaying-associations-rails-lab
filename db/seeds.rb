080c894c945f9aaf2abfa163cfda779063f07bec
Nihar Patel <niharpatel52@gmail.com>
2025-08-20 15:11:53 -0400

    Create Artists views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+beatles   = Artist.create!(name: "The Beatles")
+beyonce   = Artist.create!(name: "Beyoncé")
+drake     = Artist.create!(name: "Drake")
+taylor    = Artist.create!(name: "Taylor Swift")
+weeknd    = Artist.create!(name: "The Weeknd")
+
+Song.create!([
+  { title: "Hey Jude", artist: beatles },
+  { title: "Let It Be", artist: beatles },
+  { title: "Halo", artist: beyonce },
+  { title: "Crazy in Love", artist: beyonce },
+  { title: "God's Plan", artist: drake },
+  { title: "Hotline Bling", artist: drake },
+  { title: "Shake It Off", artist: taylor },
+  { title: "Love Story", artist: taylor },
+  { title: "Blinding Lights", artist: weeknd },
+  { title: "Save Your Tears", artist: weeknd }
+])
